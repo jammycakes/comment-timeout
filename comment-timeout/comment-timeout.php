a3c9d3f399cf178faefcb64083022dafb8e6b1d5
jammycakes <none@none>
2009-12-26 04:57:42 +0000

    Rename files (2 of 2)

---
+++ comment-timeout/comment-timeout.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Comment Timeout
+Plugin URI: http://www.jamesmckay.net/code/comment-timeout/
+Description: Automatically closes comments on blog entries after a user-configurable period of time. It has options which allow you to keep the discussion open for longer on older posts which have had recent comments accepted, or to place a fixed limit on the total number of comments in the discussion. Activate the plugin and go to <a href="options-general.php?page=comment-timeout">Options &gt;&gt; Comment Timeout</a> to configure.
+Version: 2.0.1
+Author: James McKay
+Author URI: http://www.jamesmckay.net/
+*/
+
+require_once(dirname(__FILE__) . '/class.core.php');
