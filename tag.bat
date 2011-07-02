e24cd3ce3095030b3d3d2cc50a3e95f745279bfa
James McKay <code@jamesmckay.net>
2011-07-02 13:14:38 +0100

    Add message to svn when tagging

--- tag.bat
+++ tag.bat
@@ -1,5 +1,5 @@
 if "%1"=="" goto noargs
-	svn copy http://plugins.svn.wordpress.org/comment-timeout/trunk http://plugins.svn.wordpress.org/comment-timeout/tags/%1
+	svn copy http://plugins.svn.wordpress.org/comment-timeout/trunk http://plugins.svn.wordpress.org/comment-timeout/tags/%1 -m "Tag release %1"
 	goto done:
 noargs:
 	echo Usage: tag VERSION
