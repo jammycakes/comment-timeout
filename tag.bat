139cd90ea80019cf5fa79fbd45754d035a210668
James McKay <code@jamesmckay.net>
2011-07-02 13:10:09 +0100

    Add batch file to tag versions.

---
+++ tag.bat
@@ -0,0 +1,8 @@
+if "%1"=="" goto noargs
+	svn copy http://plugins.svn.wordpress.org/comment-timeout/trunk http://plugins.svn.wordpress.org/comment-timeout/tags/%1
+	goto done:
+noargs:
+	echo Usage: tag VERSION
+	echo.
+	echo Tags the latest release in Subversion as a numbered version.
+done:
