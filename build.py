947c653b991fd10d060d9945f192c736618e0a48
James McKay <code@jamesmckay.net>
2013-08-06 17:45:56 +0100

    Refactor build scripts: extract core methods

--- build.py
+++ build.py
@@ -13,18 +13,21 @@ import shutil
 from glob import glob
 
 ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
-BUILD_DIR = os.path.join(ROOT_DIR, 'build')
-
-if os.path.isdir(BUILD_DIR):
-    shutil.rmtree(BUILD_DIR)
-
-os.mkdir(BUILD_DIR)
+BUILD_DIR = os.path.join(ROOT_DIR, 'build', 'artifact')
+SVN_DIR = os.path.join(ROOT_DIR, 'build', 'svn')
 curdir = os.getcwd()
 os.chdir(ROOT_DIR)
 
-try:
-    files = [file for spec in FILESPEC for file in glob(spec)]
+def update_version():
+    pass
 
+def remove_old_builds():
+    if os.path.isdir(BUILD_DIR):
+        shutil.rmtree(BUILD_DIR)
+
+def build_artifact():
+    os.mkdir(BUILD_DIR)
+    files = [file for spec in FILESPEC for file in glob(spec)]
     for f in files:
         src = os.path.join(ROOT_DIR, f)
         dest = os.path.join(BUILD_DIR, f)
@@ -33,5 +36,13 @@ try:
             os.mkdir(destdir)
         shutil.copyfile(src, dest)
 
+def upload_to_svn():
+    pass
+
+try:
+    update_version()
+    remove_old_builds()
+    build_artifact()
+    upload_to_svn()
 finally:
     os.chdir(curdir)
